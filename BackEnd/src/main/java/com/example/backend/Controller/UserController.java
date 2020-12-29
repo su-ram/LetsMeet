0a9e6013b328a6d4cc67b1fd54c74f5d9a3ed283
suram <swamys@naver.com>
2020-12-30 01:03:42 +0900

    키워드 별로 api 분류셋팅

---
+++ BackEnd/src/main/java/com/example/backend/Controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.backend.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/user")
+public class UserController {
+
+	@GetMapping
+	public String createUser() {
+		return "userId";
+	}
+}
