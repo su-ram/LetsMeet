5ed41f8be0187c3b0b2f79430580b1236dfb9e2f
SJLEE316 <lsj8249@naver.com>
2020-12-30 00:58:31 +0900

    CreatePlan_css and data

--- FrontEnd/src/pages/CreatePlan.js
+++ FrontEnd/src/pages/CreatePlan.js
@@ -1,14 +1,22 @@
-import * as React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
-import { Scheduler, MonthView, Toolbar, DateNavigator, TodayButton } from '@devexpress/dx-react-scheduler-material-ui';
-import "./CreatePlan.css";
+import * as React from "react";
+import Paper from "@material-ui/core/Paper";
+import { Grid } from "@material-ui/core";
+import { Header } from "../components";
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  MonthView,
+  Toolbar,
+  DateNavigator,
+  TodayButton,
+} from "@devexpress/dx-react-scheduler-material-ui";
 
 export default class CreatePlan extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
+      planName: "",
     };
   }
 
@@ -16,8 +24,11 @@ export default class CreatePlan extends React.PureComponent {
     const { data } = this.state;
 
     return (
-      <div className="create-cont">
-        <h1>언제가 좋을까요?</h1>
+      <Grid className="create-cont">
+        <Header />
+        <Grid className="create-cont-title">
+          <h2>언제가 좋을까요?🤔</h2>
+        </Grid>
         <Paper className="create-paper">
           <Scheduler data={data}>
             <ViewState defaultCurrentDate="2020-12-30" />
@@ -27,12 +38,22 @@ export default class CreatePlan extends React.PureComponent {
             <TodayButton />
           </Scheduler>
         </Paper>
-        <input 
-            className="create-name" 
-            type="text"
-            placeholder="일정 이름을 작성해주세요."/>
-        <button type="button" className="create-btn"><p className="create-btn-txt">일정 생성하기</p></button>
-      </div>
+        <input
+          className="create-name"
+          type="text"
+          value={this.state.planName}
+          onChange={(e) => {
+            this.setState({ planName: e.target.value });
+          }}
+          placeholder="일정 이름을 작성해주세요."
+        />
+        {/* <div>{this.state.planName}</div> */}
+        {/* 값이 잘 나오나 확인 */}
+        <button type="button" className="create-plan-btn">
+          일정 생성하기
+          {/* <p className="create-btn-txt">일정 생성하기</p> */}
+        </button>
+      </Grid>
     );
   }
 }
