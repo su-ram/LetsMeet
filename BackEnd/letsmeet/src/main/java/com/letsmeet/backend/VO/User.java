e0c4ea33964264aba7f40c4a4e7099ca5a5a37c3
suram <swamys@naver.com>
2020-12-30 19:54:24 +0900

    몽고디비 연결테스트 완료

---
+++ BackEnd/letsmeet/src/main/java/com/letsmeet/backend/VO/User.java
@@ -0,0 +1,14 @@
+package com.letsmeet.backend.VO;
+
+
+public class User {
+	
+	private String nick;
+	private String pass;
+	
+	public User(String nick, String pass) {
+		this.nick = nick;
+		this.pass = pass;
+	}
+
+}
