0e4b305cc6cd6f82f6c534b9d9b14c31d123a2c8
heeeun <01072417732w@gmail.com>
2021-01-14 21:24:53 +0900

    pull

--- FrontEnd/src/pages/CreatePlan.js
+++ FrontEnd/src/pages/CreatePlan.js
@@ -1,38 +1,143 @@
-import * as React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
-import { Scheduler, MonthView, Toolbar, DateNavigator, TodayButton } from '@devexpress/dx-react-scheduler-material-ui';
-import "./CreatePlan.css";
+import * as React from "react";
+import { Grid } from "@material-ui/core";
+import { Header } from "../components";
+import FormControl from "@material-ui/core/FormControl";
+import NativeSelect from "@material-ui/core/NativeSelect";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRange } from "react-date-range";
 
 export default class CreatePlan extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
+      planName: "",
+      startTime: "",
+      finishTime: "",
+      timeInterval: "",
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
     };
   }
 
+  onRangeChange = (ranges) => {
+    console.log(ranges);
+    this.setState({
+      startDate: ranges["selection"].startDate,
+      endDate: ranges["selection"].endDate,
+      key: ranges["selection"].key,
+    });
+  };
+
   render() {
-    const { data } = this.state;
+    const { data, planName, startTime, finishTime, timeInterval } = this.state;
+    //시간 배열
+    const Times = new Array();
+    for (let i = 1; i < 12; i++) {
+      Times.push(i);
+    }
+    const amTimeList = Times.map((amTime) => (
+      <option value={amTime}>오전{amTime}시</option>
+    ));
+    const pmTimeList = Times.map((pmTime) => (
+      <option value={pmTime + 12}>오후{pmTime}시</option>
+    ));
 
     return (
-      <div className="create-cont">
-        <h1>언제가 좋을까요?</h1>
-        <Paper className="create-paper">
-          <Scheduler data={data}>
-            <ViewState defaultCurrentDate="2020-12-30" />
-            <MonthView />
-            <Toolbar />
-            <DateNavigator />
-            <TodayButton />
-          </Scheduler>
-        </Paper>
-        <input 
-            className="create-name" 
-            type="text"
-            placeholder="일정 이름을 작성해주세요."/>
-        <button type="button" className="create-btn"><p className="create-btn-txt">일정 생성하기</p></button>
-      </div>
+      <Grid className="create-cont">
+        <Header />
+        <Grid className="create-cont-title">
+          <h2>언제가 좋을까요?🤔</h2>
+        </Grid>
+        <Grid className="setting">
+          {/* 캘린더 */}
+          <DateRange
+            className="create-calendar"
+            editableDateInputs={true}
+            onChange={this.onRangeChange}
+            moveRangeOnFirstSelection={false}
+            ranges={[this.state]}
+          />
+          <Grid className="create-cont-setting2">
+            {/* 일정이름 */}
+            <input
+              className="create-name"
+              type="text"
+              value={planName}
+              onChange={(e) => {
+                this.setState({ planName: e.target.value });
+              }}
+              placeholder="일정 이름을 작성해주세요."
+            />
+            {/* 시간 정하기 */}
+            <Grid className="create-time">
+              <FormControl className="create-time-start">
+                {/* <InputLabel className="timeText">Start Time</InputLabel> */}
+                <NativeSelect
+                  id="startTime"
+                  value={startTime}
+                  onChange={(e) => {
+                    this.setState({ startTime: e.target.value });
+                  }}
+                >
+                  <option aria-label="None" value="">
+                    시작시간
+                  </option>
+                  <option value="0">오전0시</option>
+                  {amTimeList}
+                  {pmTimeList}
+                </NativeSelect>
+              </FormControl>
+              <Grid>~</Grid>
+              <FormControl className="create-time-finish">
+                {/* <InputLabel className="timeText">Finish Time</InputLabel> */}
+                <NativeSelect
+                  id="finishTime"
+                  value={finishTime}
+                  onChange={(e) => {
+                    this.setState({ finishTime: e.target.value });
+                  }}
+                >
+                  <option aria-label="None" value="">
+                    끝시간
+                  </option>
+                  {amTimeList}
+                  <option value="12">오후12시</option>
+                  {pmTimeList}
+                </NativeSelect>
+              </FormControl>
+            </Grid>
+            <Grid className="create-interval">
+              <FormControl>
+                {/* <InputLabel className="timeText">Interval</InputLabel> */}
+                <NativeSelect
+                  id="timeInterval"
+                  value={timeInterval}
+                  onChange={(e) => {
+                    this.setState({ timeInterval: e.target.value });
+                  }}
+                >
+                  <option aria-label="None" value="">
+                    단위
+                  </option>
+                  <option value={15}>15분</option>
+                  <option value={30}>30분</option>
+                  <option value={60}>1시간</option>
+                </NativeSelect>
+              </FormControl>
+              <Grid>&nbsp;&nbsp;단위</Grid>
+            </Grid>
+            {/* 일정생성 버튼 */}
+            <Grid className="create-plan-btn">
+              <a href="#" className="create-plan-text">
+                일정 생성하기
+              </a>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     );
   }
 }
