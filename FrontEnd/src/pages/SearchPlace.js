699d9e1636de4e79cfcadc9621b511e709884050
SJLEE316 <lsj8249@naver.com>
2021-01-18 16:58:00 +0900

    지도에 마커Text 표시하기

--- FrontEnd/src/pages/SearchPlace.js
+++ FrontEnd/src/pages/SearchPlace.js
@@ -30,6 +30,23 @@ const SearchPlace = ({ location, match }) => {
     const map = new kakao.maps.Map(container, options);
 
     //---------------------공통 코드---------------------
+
+    //---------------------우리가 만날 위치 텍스트로 표시---------------------
+
+    var iwContent = '<div style="padding:5px;">우리가 만날 위치!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+      iwPosition = new kakao.maps.LatLng(
+        match.params.latCenter,
+        match.params.longCenter
+      ), //인포윈도우 표시 위치입니다
+      iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+    // 인포윈도우를 생성하고 지도에 표시합니다
+    var infowindow = new kakao.maps.InfoWindow({
+      map: map, // 인포윈도우가 표시될 지도
+      position: iwPosition,
+      content: iwContent,
+      removable: iwRemoveable,
+    });
   };
 
   return (
