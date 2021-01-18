f87df54d65058f4783855d37d4df821dec340e10
SJLEE316 <lsj8249@naver.com>
2021-01-18 16:07:43 +0900

    장소 검색하고 장소를 List로 나타내기

--- FrontEnd/src/components/UserInsert.js
+++ FrontEnd/src/components/UserInsert.js
@@ -8,10 +8,16 @@ import {
   Dialog,
   TextField,
 } from "@material-ui/core";
+import ListItem from "@material-ui/core/ListItem";
+import List from "@material-ui/core/List";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const { kakao } = window;
 
 const UserInsert = ({ onInsert, users }) => {
   const [dialogOpen, setdialogOpen] = useState(false);
   const [name, setName] = useState("");
+  const [results, setResults] = useState([]);
 
   // 장소 추가 버튼 누르면 Dialog open
   const handleClickOpen = () => {
@@ -54,10 +60,56 @@ const UserInsert = ({ onInsert, users }) => {
     }
   };
 
-  //----------------------검색하기 위한 버튼----------------------
+  //----------------------검색하기 위한 함수----------------------
 
-  const handleSubmit = () => {
-    console.log("검색기능");
+  const handleSubmit = (e) => {
+    // 장소 검색 객체를 생성합니다
+    var ps = new kakao.maps.services.Places();
+
+    // 키워드로 장소를 검색합니다
+    searchPlaces();
+
+    // 키워드 검색을 요청하는 함수입니다
+    function searchPlaces() {
+      var keyword = document.getElementById("input-address").value;
+
+      if (!keyword.replace(/^\s+|\s+$/g, "")) {
+        alert("키워드를 입력해주세요!");
+        return false;
+      }
+
+      // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
+      ps.keywordSearch(keyword, placesSearchCB);
+    }
+
+    // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
+    function placesSearchCB(data, status, pagination) {
+      if (status === kakao.maps.services.Status.OK) {
+        // 정상적으로 검색 완료
+        getListItem(data); // List로 나타내기
+      } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+        alert("검색 결과가 존재하지 않습니다.");
+        return;
+      } else if (status === kakao.maps.services.Status.ERROR) {
+        alert("검색 결과 중 오류가 발생했습니다.");
+        return;
+      }
+    }
+  };
+
+  //----------------------검색 결과 주소들을 결과로 반환 -> List로 나타내기 위함----------------------
+
+  let datas = [];
+  const getListItem = (places) => {
+    // console.log(places);
+    datas = places;
+    setResults(datas);
+  };
+
+  //----------------------유저 이름과 검색한 주소를 UserList에 삽입하기----------------------
+
+  const handleAddressChange = (address, e) => {
+    console.log("이름, 주소 UserList에 삽입하기");
   };
 
   return (
@@ -93,6 +145,21 @@ const UserInsert = ({ onInsert, users }) => {
               }
             }}
           />
+          {/* kakao api 주소 나타내는 영역 */}
+          <List component="nav" aria-label="contacts">
+            {results.map((address, i) => (
+              <ListItem
+                button
+                onClick={(e) => handleAddressChange(address, e)} //why?
+                key={i}
+              >
+                <ListItemText
+                  primary={address.place_name}
+                  secondary={address.address_name}
+                />
+              </ListItem>
+            ))}
+          </List>
         </Grid>
       </Dialog>
     </div>
