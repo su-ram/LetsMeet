e6ae975cefc7e3710ac372b2404db3b1e42582b8
SJLEE316 <lsj8249@naver.com>
2021-01-18 16:57:07 +0900

    지도 생성

--- FrontEnd/src/pages/SearchPlace.js
+++ FrontEnd/src/pages/SearchPlace.js
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import queryString from "query-string";
 import { Grid } from "@material-ui/core";
 import "../scss/pages/_SearchPlace.scss";
+
+const { kakao } = window;
 
 const SearchPlace = ({ location, match }) => {
   const query = queryString.parse(location.search);
   console.log(query);
 
+  //---------------------지도 생성하기---------------------
+
+  useEffect(() => {
+    mapScript();
+  }, []);
+
+  const mapScript = () => {
+    let container = document.getElementById("map");
+    let options = {
+      center: new kakao.maps.LatLng(
+        match.params.latCenter,
+        match.params.longCenter
+      ),
+      level: 7,
+    };
+
+    //---------------------공통 코드---------------------
+
+    const map = new kakao.maps.Map(container, options);
+
+    //---------------------공통 코드---------------------
+  };
+
   return (
-    <Grid>
-      위도는 {match.params.latCenter}, 경도는 {match.params.longCenter}
-    </Grid>
+    <>
+      <Grid>우리 어디서 만날까? 바로 여기!</Grid>
+      <Grid>
+        위도는 {match.params.latCenter}, 경도는 {match.params.longCenter}
+      </Grid>
+      <Grid className="map_wrap">
+        <Grid id="map">map here!</Grid> {/* 지도 오는 곳 */}
+        <Grid className="hAddr">
+          <Grid className="title">지도중심기준 행정동 주소정보</Grid>
+          <Grid id="centerAddr"></Grid>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
