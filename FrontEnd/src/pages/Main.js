7853e04001535df511c38b50888bd74f4da0f802
SJLEE316 <lsj8249@naver.com>
2021-01-18 15:21:09 +0900

    component PlaceMain

--- FrontEnd/src/pages/Main.js
+++ FrontEnd/src/pages/Main.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { CreatePlan, ManagePlan } from ".";
+import { CreatePlan, ManagePlan, PlaceMain } from ".";
 
 const Main = () => {
   return (
     <>
       <Switch>
         <Route exact path="/Create" component={CreatePlan} />
+        <Route exact path="/Place" component={PlaceMain} />
         <Route exact path="/:url" component={ManagePlan} />
       </Switch>
     </>
