94ce4a30fddf358afc08583da67393af7e16d714
Cho-Eunhak <02069@naver.com>
2021-01-19 15:09:03 +0900

    Revert "가이드 추가"
    This reverts commit 689a6e5fa9383ecad6e1d2576a33acb153d1d288.

--- FrontEnd/src/components/Header.js
+++ FrontEnd/src/components/Header.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-import {Link} from "react-router-dom"
+
 const Header = () => (
   <Grid className="header-rule">
     <Grid className="header-logo">
       <img src="/img/letsmeet.png" alt="logo" />
     </Grid>
     <Grid className="goto-manual">
-    <a href="/Guide">사용법 바로가기</a>
+      <a href="#">사용법 바로가기</a>
     </Grid>
   </Grid>
 );
 
 export default Header;
-//<Link to="/Guide" style={{ textDecoration: 'none', color: 'black' }}>사용법 바로가기</Link>
--- FrontEnd/src/pages/index.js
+++ FrontEnd/src/pages/index.js
@@ -1,4 +1,3 @@
 export {default as Main} from './Main';
 export {default as CreatePlan} from './CreatePlan';
 export {default as ManagePlan} from './ManagePlan';
-export {default as Guide} from './Guide';
