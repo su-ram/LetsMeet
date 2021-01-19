689a6e5fa9383ecad6e1d2576a33acb153d1d288
Cho-Eunhak <02069@naver.com>
2021-01-19 14:15:52 +0900

    가이드 추가

---
+++ FrontEnd/src/pages/Guide.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Guide = () => {
+
+    return (
+        <div className="guide-con">
+            <img className="guide-img" src="/img/guide.png" />
+        </div>
+    )
+    }
+    
+    export default Guide;
+    
--- FrontEnd/src/pages/index.js
+++ FrontEnd/src/pages/index.js
@@ -1,3 +1,4 @@
 export {default as Main} from './Main';
 export {default as CreatePlan} from './CreatePlan';
 export {default as ManagePlan} from './ManagePlan';
+export {default as Guide} from './Guide';
