1f6b75bc56d88994b84d4ada1d27eb9a7b525007
Cho-Eunhak <02069@naver.com>
2021-01-19 15:20:55 +0900

    실수로 커밋 revert 눌러서 가이드 다시 추가했습니다...

---
+++ FrontEnd/src/pages/Guide.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Guide = () => {
+
+    return (
+        <div className="guide-con">
+            <img className="guide-img" src="/img/guide.png" />
+        </div>
+    )
+    }
+
+    export default Guide;
