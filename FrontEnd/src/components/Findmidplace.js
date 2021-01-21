aba9231c4958e8fc1e050128f1432892fbedfc55
JaneseoHyeon <49301073+JaneseoHyeon@users.noreply.github.com>
2021-01-21 15:34:59 +0900

    중간지점, plan 생성 페이지 링크 연결

--- FrontEnd/src/components/Findmidplace.js
+++ FrontEnd/src/components/Findmidplace.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 const Findmidplace = () => {
 
+const goToPlace = () => {
+    window.open('/Place');
+};
+
 return (
     <div>
          <div className="title">중간지점 찾기 
          <img className="img" alt='top3' src="/img/top3.png"></img>
          <span className="midbtn">
-         <Button variant="contained" color="primary" >어디서 만날까?</Button>
+         <Button variant="contained" color="primary" onClick={goToPlace}>어디서 만날까?</Button>
          </span>
          </div>
     </div>
